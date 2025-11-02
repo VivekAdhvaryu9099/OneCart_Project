d50f1b6e41129483b4ae68967f6bc57bccfc81a8
VivekAdhvaryu9099 <adhvaryuvivek45@gmail.com>
2025-11-02 11:35:12 +0530

    Add files via upload

---
+++ Admin/src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import  Home  from './pages/Home'
+import Add from './pages/Add'
+import List from './pages/List'
+import Order from './pages/Order'
+import Login from './pages/Login'
+import { useContext } from 'react'
+import { adminDataContext } from './context/AdminContext'
+const App = () => {
+  let {adminData} = useContext(adminDataContext)
+  return (
+    <>
+       {!adminData  ? <Login/> :<>
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/add' element={<Add/>} />
+            <Route path='/list' element={<List/>} />
+            <Route path='/order' element={<Order/>} />
+            <Route path='/login' element={<Login/>} />
+        </Routes>
+        
+        </>
+        }
+    </>
+  )
+}
+
+export default App
